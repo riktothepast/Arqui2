6af243f6c3466fbf61f7e0bf5274b6d9637a54e6
riktothepast <rik@502studios.net>
2015-02-12 10:33:07 -0600

    adds git ignore and falling cube via script

---
+++ Unity/Script_FallingCube/Assets/Gravedad.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravedad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x, transform.position.y - 10f *Time.deltaTime, transform.position.z);
+	}
+}

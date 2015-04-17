e7b96ec1e9bd9ce6e8acc814670695cd26c25eeb
ritothepast <rik@502studios.net>
2015-04-17 10:14:46 -0600

    Adds bouncy spheres test.
    - materials
    - bounciness modifier (via script)
    - force applying script

---
+++ Unity/BouncyTest/Boncyness/Assets/Scripts/SphereScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereScript : MonoBehaviour {
+
+    // this value changes how much the ball will bounce
+    public float bouncyFactor;
+    // how much force should we give to the sphere?
+    public Vector3 initialForce;
+	
+    // Use this for initialization
+	void Start () {
+
+        // get the GO phyciscs material and change its bouncyFactor.
+        GetComponent<SphereCollider>().material.bounciness = bouncyFactor;
+
+        //lets add some force and a force mode
+        GetComponent<Rigidbody>().AddForce(initialForce, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

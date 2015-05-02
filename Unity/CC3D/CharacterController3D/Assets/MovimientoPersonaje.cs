71aad758f22bfa2963ab02c069ca2460ada4d3cc
riktothepast <rik@502studios.net>
2015-05-02 13:11:55 -0600

    Adds character controller 3D example project.

---
+++ Unity/CC3D/CharacterController3D/Assets/MovimientoPersonaje.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovimientoPersonaje : MonoBehaviour {
+
+    Vector3 velocity;
+    public float gravity;
+    public float jumpImpulse;
+    public float movementSpeed;
+    CharacterController characterController;
+
+	// Use this for initialization
+	void Start () {
+        characterController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        velocity.y += gravity * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            velocity.y = jumpImpulse;
+        }
+
+        velocity.x = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        velocity.z = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+
+        characterController.Move(velocity);
+	}
+}
